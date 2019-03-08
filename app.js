a2f855a090d4f305b4550adf8dc2e5c9f189d391
Morgan Dorfman <mdorfman21@gmail.com>
2019-03-07 22:11:11 -0500

    final javascript

--- app.js
+++ app.js
@@ -63,6 +63,7 @@ function getCocktailAPI() {
 
         console.log("TEST ME", results);
         database.ref(IDofDrink).push({
+          strID: results.idDrink,
           strDrink: results.strDrink,
           strCategory: results.strCategory,
           strGlass: results.strGlass,
@@ -72,51 +73,12 @@ function getCocktailAPI() {
         });
       });
     });
-
-    //get the first 10 results and write them to the screen
-    // for (var i = 0; i < 10; i++) {
-    //   var card = $("<div>");
-    //   var drinkImage = $("<img>");
-    //   var drinkTitle = $("<p>");
-    //   card.addClass("card");
-    //   drinkImage.attr("src", results[i].strDrinkThumb);
-    //   drinkTitle.text(results[i].strDrink)
   });
 }
-// database.ref().on("value", function(snapshot) {
-//   var results = snapshot.val();
-//   console.log("LOOK AT THIS", results);
-
-//   var recipes = Object.values(results);
-//   console.log(recipes);
-
-//   // for (var property1 in results) {
-//   //   if (results.hasOwnProperty(property1)) {
-//   //     string1 = results[property1];
-//   //     string1.id = property1;
-//   //     string1.ingredients = [];
-//   //     recipes.push(string1);
-//   //     console.log(string1);
-//   //   }
-//   // }
-//   const filteredRecipes = recipes.filter(recipe => {
-//     var found = recipe.ingredients.find(
-//       ingredient =>
-//         ingredient.name ===
-//         $("#category")
-//           .val()
-//           .trim()
-//     );
-
-//     if (found) {
-//       return recipe;
-//     }
-//   });
-//   console.log(filteredRecipes);
-// });
 
 $("#submit").on("click", function(e) {
   e.preventDefault();
+  $("#recipe-output").empty();
   var searchByTerms = [];
   $("#div-that-holds-checkboxes")
     .find("input[type='checkbox']")
@@ -134,16 +96,6 @@ $("#submit").on("click", function(e) {
     var recipes = Object.values(results);
     console.log(recipes);
 
-    // const filteredRecipes = recipes.filter(recipe => {
-    //   var found = recipe.ingredients.find(ingredient =>
-    //     ingredient.name.includes(searchByTerms)
-    //   );
-
-    //   if (found) {
-    //     return recipe;
-    //   }
-    // });
-
     var testFilter = recipes.filter(recipe => {
       var trueStatus = true;
       for (var i = 0; i < searchByTerms.length; i++) {
@@ -155,38 +107,55 @@ $("#submit").on("click", function(e) {
           break;
         }
       }
-      // console.log(trueStatus);
 
       return trueStatus;
     });
     console.log(testFilter);
-
-    for (var i = 0; i < 10; i++) {
+    var promiseArray = [];
+    for (var i = 0; i < 6; i++) {
       if (!testFilter[i]) {
         break;
       } else {
-        var y = testFilter[i];
+        var promise = $.ajax({
+          url:
+            "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" +
+            testFilter[i].strID,
+          method: "GET"
+        });
+        promiseArray.push(promise);
+      }
+    }
+    console.log(promiseArray);
+    Promise.all(promiseArray).then(function(resolvedPromises) {
+      resolvedPromises.forEach((resolvedPromise, index) => {
+        var y = testFilter[index];
+        var drinkImage = $("<img>");
         var name = $("<p>");
-        var howTo = $("<p>");
+        var howTo = $("<p>").addClass("drink-howTo");
         var instructions = $("<p>");
-        var parentDiv = $("<div>");
+        var parentDiv = $("<div>").addClass("card");
         var drinkNumber = $("<p>");
-        name.text(y.strDrink);
-        instructions.text(y.strInstructions);
-        drinkNumber.text("Drink #" + (i + 1));
+
+        drinkImage
+          .attr("src", resolvedPromise.drinks[0].strDrinkThumb)
+          .addClass("drink-images");
+        name.text(y.strDrink).addClass("drink-name");
+        instructions.text(y.strInstructions).addClass("drink-instructions");
+        drinkNumber.text("Drink #" + (index + 1)).addClass("drinkNumber");
         y.ingredients.forEach(z => {
           var comboMeasure = $("<p>");
-          comboMeasure.text(z.measurement + z.name);
+          comboMeasure
+            .text(z.measurement + "" + z.name)
+            .addClass("drink-comboMeasure");
           howTo.append(comboMeasure);
         });
         parentDiv.append(drinkNumber);
         parentDiv.append(name);
+        parentDiv.append(drinkImage);
         parentDiv.append(howTo);
         parentDiv.append(instructions);
         $("#recipe-output").append(parentDiv);
-      }
-    }
+      });
+    });
   });
-
-  // console.log(filteredRecipes);
 });
